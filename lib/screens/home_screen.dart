2dfb92e9e9dee5f9ee8ffdf7dcffff292a9fcb93
ch-hyungoh <67888488+ch-hyungoh@users.noreply.github.com>
2023-12-01 16:44:41 +0900

    fimal

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,17 +10,72 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
+  bool isRunnig = false;
+  int totalPomodoros = 0;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isRunnig = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    // duration(주기마다) 뒤에 함수를 실행한다
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+    setState(() {
+      isRunnig = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunnig = false;
+    });
+  }
+
+  void resetPressed() {
+    timer.cancel();
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+    });
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    print(duration.toString());
+    print(duration.toString().split("."));
+    print(duration.toString().split(".").first);
+    print(duration.toString().split(".").first.substring(2, 7));
+    return duration.toString().split('.').first.substring(2, 7);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Column(children: [
         Flexible(
-          flex: 1,
+          flex: 2,
           child: Container(
             alignment: Alignment.bottomCenter,
             child: Text(
-              "25:00",
+              format(totalSeconds),
               style: TextStyle(
                 color: Theme.of(context).cardColor,
                 fontSize: 89,
@@ -29,13 +86,27 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         Flexible(
           flex: 3,
-          child: Center(
-            child: IconButton(
-              iconSize: 120,
-              color: Theme.of(context).cardColor,
-              onPressed: () {},
-              icon: const Icon(Icons.play_circle_outline),
-            ),
+          child: Column(
+            children: [
+              Center(
+                child: IconButton(
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                  onPressed: isRunnig ? onPausePressed : onStartPressed,
+                  icon: Icon(isRunnig
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outline),
+                ),
+              ),
+              Center(
+                child: IconButton(
+                  iconSize: 40,
+                  color: Theme.of(context).cardColor,
+                  onPressed: resetPressed,
+                  icon: const Icon(Icons.restore),
+                ),
+              ),
+            ],
           ),
         ),
         Flexible(
@@ -46,6 +117,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: Container(
                   decoration: BoxDecoration(
                     color: Theme.of(context).cardColor,
+                    borderRadius: BorderRadius.circular(50),
                   ),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -61,7 +133,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 .color),
                       ),
                       Text(
-                        '0',
+                        '$totalPomodoros',
                         style: TextStyle(
                             fontSize: 60,
                             fontWeight: FontWeight.w600,
